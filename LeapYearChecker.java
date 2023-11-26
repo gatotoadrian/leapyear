8ff31080b2f4e2d6156e1d4fc91441a62ee480f5
Shaban <Adrianeddie8113>
2023-11-26 13:36:09 +0300

    first commit

---
+++ LeapYearChecker.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the year: ");
+        int year = input.nextInt();
+        
+        if (isLeapYear(year)) {
+            System.out.println("The year you entered is a leap year.");
+        } else {
+            System.out.println("The year you entered is not a leap year.");
+        }
+    }
+    
+    public static boolean isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return true;
+        } else {
+            return false;
+            
+        }
+    }
+}
